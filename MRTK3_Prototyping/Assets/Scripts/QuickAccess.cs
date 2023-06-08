37d0245a82a889161bd56a83701b248e650cd996
Brussell03 <r.brussell03@gmail.com>
2023-06-08 18:37:11 -0500

    Auto close quick access menus

--- MRTK3_Prototyping/Assets/Scripts/QuickAccess.cs
+++ MRTK3_Prototyping/Assets/Scripts/QuickAccess.cs
@@ -1,22 +1,34 @@
+using Microsoft.MixedReality.Toolkit.UX;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class QuickAccess : MonoBehaviour
 {
-	public RectTransform reference;
+	public GameObject reference;
+	[Tooltip("Seconds until quick access bar automatically closes")]
+	public float closeTime = 10;
+
 	private RectTransform rt;
+	private float openTime;
+	public bool opened { get; set; }
 
 	// Start is called before the first frame update
 	void Start()
     {
 		rt = gameObject.GetComponent<RectTransform>();
-		rt.localPosition = new Vector3(reference.localPosition.x, rt.localPosition.y, rt.localPosition.z);
+		rt.localPosition = new Vector3(reference.GetComponent<RectTransform>().localPosition.x, rt.localPosition.y, rt.localPosition.z);
 	}
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (opened && Time.time - openTime >= closeTime) {
+			reference.GetComponent<PressableButton>().ForceSetToggled(false);
+		}
     }
+
+	public void SetOpenTime() {
+		openTime = Time.time;
+	}
 }
