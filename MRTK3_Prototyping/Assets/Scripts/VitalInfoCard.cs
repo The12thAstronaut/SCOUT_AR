0a594c5da94aeeb0cd6a164f9993b986fe66e2c8
Brussell03 <r.brussell03@gmail.com>
2023-06-10 05:48:33 -0500

    Populated suit vitals menu

---
+++ MRTK3_Prototyping/Assets/Scripts/VitalInfoCard.cs
@@ -0,0 +1,108 @@
+using Microsoft.MixedReality.Toolkit.UX;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.XR.CoreUtils;
+using UnityEngine;
+using UnityEngine.UI;
+using Slider = Microsoft.MixedReality.Toolkit.UX.Slider;
+
+public class VitalInfoCard : MonoBehaviour
+{
+	public float nominalMax;
+	public float nominalMin;
+	public float errorMax;
+	public float errorMin;
+
+	public Color errorColor = new Color(188, 0, 0);
+	public Color goodColor = new Color(0, 159, 15);
+	private Material fillMat;
+	private Color originalColor;
+
+	private float maxVal => nominalMax > errorMax ? nominalMax : errorMax;
+	private float minVal => nominalMin < errorMin ? nominalMin : errorMin;
+	private float normalizedValue;
+
+	public Slider slider;
+	public TextMeshProUGUI minText;
+	public TextMeshProUGUI maxText;
+	public TextMeshProUGUI errorText;
+	public TextMeshProUGUI currentText;
+	public TextMeshProUGUI valueText;
+	public TextMeshProUGUI nameText;
+	public string vitalName;
+	public string decimalFormat = "0.";
+
+	private float sliderWidth;
+	private RectTransform valueRT;
+
+	public float value;
+
+	// Start is called before the first frame update
+	void Start()
+    {
+		sliderWidth = slider.gameObject.GetComponent<RectTransform>().sizeDelta.x;
+		valueRT = currentText.gameObject.GetComponent<RectTransform>();
+
+		minText.text = minVal.ToString(decimalFormat);
+		maxText.text = maxVal.ToString(decimalFormat);
+		currentText.text = value.ToString(decimalFormat);
+		valueText.text = currentText.text;
+		nameText.text = vitalName;
+
+
+		RectTransform errorRT = errorText.gameObject.GetComponent<RectTransform>();
+		if (nominalMax > errorMax) {
+			errorText.text = errorMax.ToString(decimalFormat);
+			errorRT.localPosition = new Vector3(((errorMax - minVal) / (maxVal - minVal) - 0.5f) * sliderWidth, errorRT.localPosition.y, errorRT.localPosition.z);
+
+			maxText.color = goodColor;
+			maxText.transform.GetChild(0).GetComponent<TextMeshProUGUI>().color = goodColor;
+			minText.color = errorColor;
+			minText.transform.GetChild(0).GetComponent<TextMeshProUGUI>().color = errorColor;
+		} else {
+			errorText.text = errorMin.ToString(decimalFormat);
+			errorRT.localPosition = new Vector3(((errorMin - minVal) / (maxVal - minVal) - 0.5f) * sliderWidth, errorRT.localPosition.y, errorRT.localPosition.z);
+		}
+
+		valueRT.localPosition = new Vector3(((value - minVal) / (maxVal - minVal) - 0.5f) * sliderWidth, valueRT.localPosition.y, valueRT.localPosition.z);
+
+		fillMat = Instantiate<Material>(slider.gameObject.GetNamedChild("SliderTrack").transform.GetChild(0).GetComponent<RawImage>().material);
+		slider.gameObject.GetNamedChild("SliderTrack").transform.GetChild(0).GetComponent<RawImage>().material = fillMat;
+
+		originalColor = fillMat.GetColor("_Color");
+		UpdateSlider();
+	}
+
+	// Update is called once per frame
+	void Update() {
+		value = minVal + Mathf.PingPong(Time.time * (maxVal - minVal) / 4, maxVal - minVal);
+
+		valueRT.localPosition = new Vector3(((value - minVal) / (maxVal - minVal) - 0.5f) * sliderWidth, valueRT.localPosition.y, valueRT.localPosition.z);
+		currentText.text = value.ToString(decimalFormat);
+		valueText.text = currentText.text;
+		UpdateSlider();
+	}
+
+	private void ClampVal() {
+		if (value > maxVal) {
+			value = maxVal;
+		} else if (value < minVal) {
+			value = minVal;
+		}
+	}
+
+	private void UpdateSlider() {
+		ClampVal();
+		normalizedValue = (value - minVal) / (maxVal - minVal);
+		slider.Value = normalizedValue;
+
+		if (nominalMax > errorMax && value <= errorMax) {
+			fillMat.SetColor("_Color", errorColor);
+		} else if (nominalMax < errorMax && value >= errorMin) {
+			fillMat.SetColor("_Color", errorColor);
+		} else {
+			fillMat.SetColor("_Color", originalColor);
+		}
+	}
+}
