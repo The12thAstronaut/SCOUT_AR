d2641774ad0e80ce380526c381f335ee78350584
Brussell03 <r.brussell03@gmail.com>
2023-08-14 19:39:36 -0500

    Fixed scroll views' contents' rotation.

--- MRTK3_Prototyping/Assets/Scripts/MarkerInfoCard.cs
+++ MRTK3_Prototyping/Assets/Scripts/MarkerInfoCard.cs
@@ -15,7 +15,7 @@ public class MarkerInfoCard : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-		transform.rotation = Quaternion.identity;
+		transform.localRotation = Quaternion.identity;
     }
 
 	// Update is called once per frame
--- MRTK3_Prototyping/Assets/Scripts/MarkerSelectorButton.cs
+++ MRTK3_Prototyping/Assets/Scripts/MarkerSelectorButton.cs
@@ -10,7 +10,7 @@ public class MarkerSelectorButton : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-		transform.rotation = Quaternion.identity;
+		transform.localRotation = Quaternion.identity;
 	}
 
     // Update is called once per frame
